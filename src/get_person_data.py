6a60c5609573a4126c69ba1a8099dc81d96f13e2
cKryn <tapalia.roberto.gabriel@gmail.com>
2024-11-18 20:37:21 +0200

    main file added

--- src/get_person_data.py
+++ src/get_person_data.py
@@ -45,10 +45,5 @@ def login():
     pass
 
 
-if __name__ == "__main__":
-    try:
-        person = get_name_and_age(112)
-        print(person)
-    except KeyError:
-        print("Not a good user ID")
+
 
