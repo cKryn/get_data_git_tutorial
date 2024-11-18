8c5224b819ca5f378c9965b793b5f37ea8f9985f
cKryn <tapalia.roberto.gabriel@gmail.com>
2024-11-18 19:18:17 +0200

    More functionality

---
+++ input/get_person_data.py
@@ -0,0 +1,53 @@
+"""Get person data"""
+import pandas as pd
+
+DATA = {
+    111: {
+        "name": "Dan",
+        "age": 20
+    },
+    112: {
+        "name": "Maria",
+        "age": 33
+    }
+
+}
+
+
+def get_name_and_age(person_id):
+    """Return person name and age"""
+    return DATA[person_id]
+
+
+
+def save_data(file, person_data):
+    """Save person data to file"""
+    with open(file, "w") as fw:
+        fw.write(person_data)
+
+
+
+def read_data(file):
+    """Return a list with all rows from file"""
+    with open(file, "r") as fr:
+        content = fr.readlines()
+
+    return content
+
+
+def read_csv_data(file) -> pd.DataFrame:
+    """Return data from csv"""
+    return pd.read_csv(file)
+
+
+def login():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        person = get_name_and_age(112)
+        print(person)
+    except KeyError:
+        print("Not a good user ID")
+
